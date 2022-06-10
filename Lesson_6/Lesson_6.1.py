88ac6b598692aabe0770e5a81dc4d02d411584d4
Maksim Sherstobitov <sherstobitov.maxim@yandex.ru>
2022-06-11 01:09:45 +0500

    Lesson_6.1

---
+++ Lesson_6/Lesson_6.1.py
@@ -0,0 +1,16 @@
+
+def parse_log(file):
+
+    if file:
+        with open(file,"r",encoding="utf-8") as file:
+            for line in file:
+                ip = line.split(" - - ")[0]
+                rsp_and_pth = line.split('"')[1]
+                rsp = rsp_and_pth.split()[0]
+                pth = rsp_and_pth.split()[1]
+                yield(ip, rsp, pth)
+
+if __name__ == "__main__":
+   logs = parse_log("./nginx_logs.txt")
+   for i in logs:
+       print(i)
